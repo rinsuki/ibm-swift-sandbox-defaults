f71ff7efd6498942ecdaa250f145d0c299f59c2d
りんすき <6533808+rinsuki@users.noreply.github.com>
2017-12-01 20:15:53 +0900

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
     name: "ibm-swift-sandbox-defaults",
     dependencies: [
-        .Package(url: "https://github.com/IBM-Swift/SwiftyJSON.git", versions: Version(1, 0, 0)..<Version(3, .max, .max)),
+        .Package(url: "https://github.com/IBM-Swift/SwiftyJSON.git", versions: Version(1, 0, 0)..<Version(17, .max, .max)),
     ]
 )
